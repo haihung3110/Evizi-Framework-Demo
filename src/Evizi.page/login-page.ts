5204c7f761c0a907337100cbefce3618152f1959
haihung3110 <49674828+haihung3110@users.noreply.github.com>
2022-10-19 09:25:23 +0700

    third

--- src/Evizi.page/login-page.ts
+++ src/Evizi.page/login-page.ts
@@ -1,1 +1,31 @@
 import { Builder, By, Key, until, WebDriver } from "selenium-webdriver";
+
+let driver: WebDriver;
+
+export class loginPage {
+  async goToLoginPage() {
+    await driver.get(
+      "https://id.atlassian.com/login?continue=https%3A%2F%2Fid.atlassian.com%2Fjoin%2Fuser-access%3Fresource%3Dari%253Acloud%253Ajira%253A%253Asite%252F7faa1f50-6795-4580-990d-f2cdb97e0605%26continue%3Dhttps%253A%252F%252Fhungphan-training-fresher.atlassian.net&application=jira"
+    );
+  }
+
+  async setUserName(userName: string) {
+    await driver
+      .findElement(By.xpath(`//input[@id='username']`))
+      .sendKeys(userName);
+  }
+
+  async clickContinueBtn() {
+    await driver.findElement(By.xpath(`//button[@id='login-submit']`)).click();
+  }
+
+  async setUserPassword(password: string) {
+    await driver
+      .findElement(By.xpath(`//input[@id='password']`))
+      .sendKeys(password);
+  }
+
+  async clickLoginBtn() {
+    await driver.findElement(By.xpath(`//button[@id='login-submit']`)).click();
+  }
+}
