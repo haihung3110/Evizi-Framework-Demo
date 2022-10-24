836b536b66f49f8336613d29c828fb4432454172
haihung3110 <49674828+haihung3110@users.noreply.github.com>
2022-10-24 14:39:34 +0700

    remove cucumber-tsflow and using cucumber

---
+++ features/create_project.feature
@@ -0,0 +1,7 @@
+Feature: Create a new project
+
+  Scenario: Create new project
+    Given In home page
+    When User click 
+    When User enter "Hungphan3110" and click log in button
+    Then User enter your work page
---
+++ src/step-definitions/login_page.step.ts
@@ -0,0 +1,39 @@
+
+import { Given, When, Then } from "cucumber";
+import { Builder, WebDriver } from "selenium-webdriver";
+import { Login } from "../page/login";
+import { Home } from "../page/home";
+import assert from "assert";
+
+require("chromedriver");
+
+let driver: WebDriver;
+var { setDefaultTimeout } = require("cucumber");
+setDefaultTimeout(60 * 1000);
+
+Given(/^Go to page Jira "([^"]*)"$/, async (url: string) => {
+  driver = new Builder().forBrowser("chrome").build();
+  await driver.get(`${url}`);
+});
+
+When(
+  /User enter "([^"]*)" and click continue button/,
+  async (userName: string) => {
+    let loginPage = new Login(driver);
+    await loginPage.setUserName(userName);
+  }
+);
+
+When(
+  /User enter "([^"]*)" and click log in button/,
+  async function (userPassword: string) {
+    let loginPage = new Login(driver);
+    await loginPage.setUserPassword(userPassword);
+  }
+);
+
+Then(/User enter your work page/, async function isUserOnWorkPage() {
+  let homePage = new Home(driver);
+  let isOnHomePage = await homePage.isAt();
+  assert.equal(isOnHomePage, true);
+});
