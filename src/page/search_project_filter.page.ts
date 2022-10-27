ee750a5fb6f76bcde050c79aa022fafa74b3203f
haihung3110 <49674828+haihung3110@users.noreply.github.com>
2022-10-27 22:38:51 +0700

    change name file

---
+++ src/page/search_project_filter.page.ts
@@ -0,0 +1,49 @@
+import { By, WebDriver } from "selenium-webdriver";
+
+export class SearchProjectByFilterPage {
+  driver: WebDriver;
+
+  constructor(driver: WebDriver) {
+    this.driver = driver;
+  }
+  // element for SearchProjectByFilterPage
+  projectsTopHeadBtn = By.xpath(`//button[.='Projects']`);
+
+  viewAllProjectsPageBtn = By.xpath(`//a[.='View all projects']`);
+
+  filterProjects = By.xpath(`//div[text()='All Jira products']`);
+
+  itemFromFilter = By.xpath(`//div[@id='react-select-17-option-3']`);
+
+  projectNameLabel = By.xpath(
+    `//nav[@aria-label='Breadcrumbs']//following::li//span`
+  );
+
+  filterLabel = By.xpath(`//div[text()='Jira Work Management']`)
+
+  // method for search project by filter
+
+  public async isCurrentProjectCreated() {
+    await this.driver.findElement(this.projectNameLabel).isDisplayed();
+  }
+
+  public async clickProjectsTopHead() {
+    await this.driver.findElement(this.projectsTopHeadBtn).click();
+  }
+
+  public async clickViewAllProjectBtn() {
+    await this.driver.findElement(this.viewAllProjectsPageBtn).click();
+  }
+
+  public async openFilterProjectsItems() {
+    await this.driver.findElement(this.filterProjects).click();
+  }
+
+  public async selectItemFormFilter() {
+    await this.driver.findElement(this.itemFromFilter).click();
+  }
+
+  public async isDisplayProjectsByFilter(){
+    await this.driver.findElement(this.filterLabel).isDisplayed()
+  }
+}
---
+++ src/step-definitions/search_project_filter.step.ts
@@ -0,0 +1,35 @@
+import { Given, When, Then, World } from "cucumber";
+import { SearchProjectByFilterPage } from "../page/search_project_filter.page";
+
+require("chromedriver");
+
+let searchProjectsByFilter: SearchProjectByFilterPage;
+
+//define step for SearchProjectByFilterStep
+Given(/^User is on project was created/, async function (this: World) {
+  searchProjectsByFilter = new SearchProjectByFilterPage(this.driver);
+  await searchProjectsByFilter.isCurrentProjectCreated();
+});
+
+When(/^Select Projects button in top header/, async function (this: World) {
+  searchProjectsByFilter = new SearchProjectByFilterPage(this.driver);
+  await searchProjectsByFilter.clickProjectsTopHead();
+  await this.driver.sleep(3000);
+});
+
+When(
+  /^User select view all projects item from drop down menu/,
+  async function (this: World) {
+    searchProjectsByFilter = new SearchProjectByFilterPage(this.driver);
+    await searchProjectsByFilter.clickViewAllProjectBtn();
+    await this.driver.sleep(5000);
+  }
+);
+
+Then(
+  /^All project with type "Jira Work Management" displays/,
+  async function (this: World) {
+    searchProjectsByFilter = new SearchProjectByFilterPage(this.driver);
+    await searchProjectsByFilter.isDisplayProjectsByFilter();
+  }
+);
