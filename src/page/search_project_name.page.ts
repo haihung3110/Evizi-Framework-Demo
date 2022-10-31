d981c62a3d38d8f37058a15ed9f94c7327cb45e1
haihung3110 <49674828+haihung3110@users.noreply.github.com>
2022-10-31 20:28:24 +0700

    fix conflict

---
+++ src/page/search_project_name.page.ts
@@ -0,0 +1,33 @@
+import { WebDriver, By } from "selenium-webdriver";
+
+export class SearchProjectsByNamePage {
+  driver: WebDriver;
+
+  constructor(driver: WebDriver) {
+    this.driver = driver;
+  }
+
+  //implement for locator for SearchProjectsByName
+
+  searchField = By.xpath(`//input[@name='search']`);
+
+  isProjectsDisplayed = By.xpath(
+    `//tr[td[.='']]//div[.='Evizi-Hung-Training']`
+  );
+
+  //implement method for SearchProjectsByName
+
+  public async isCurrentProjectPage() {
+    await this.driver.findElement(this.isProjectsDisplayed).isDisplayed();
+  }
+
+  public async enterNameProject(name: string) {
+    await this.driver.findElement(this.searchField).sendKeys(name);
+  }
+
+  public async isProjectNameDisplayed(nameProjectInSearchField: string) {
+    await this.driver
+      .findElement(this.isProjectsDisplayed)
+      .sendKeys(nameProjectInSearchField);
+  }
+}
