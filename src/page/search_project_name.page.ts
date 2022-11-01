fa9fdd495b60cbccb26697bbbcd1bbadfc67f916
haihung3110 <49674828+haihung3110@users.noreply.github.com>
2022-11-01 10:51:08 +0700

    done PRO_03

--- src/page/search_project_name.page.ts
+++ src/page/search_project_name.page.ts
@@ -1,33 +1,40 @@
 import { WebDriver, By } from "selenium-webdriver";
+import { SeleniumWebdriverWrapper } from "../common/seleniumWebdriverWrapper";
 
-export class SearchProjectsByNamePage {
-  driver: WebDriver;
-
+export class SearchProjectsByNamePage extends SeleniumWebdriverWrapper {
   constructor(driver: WebDriver) {
-    this.driver = driver;
+    super(driver);
   }
 
   //implement for locator for SearchProjectsByName
 
+  projectsTopHeadBtn = By.xpath(`//button[.='Projects']`);
+
+  viewAllProjectsPageBtn = By.xpath(
+    `//div[@role='group']//a[@href='/jira/projects']`
+  );
+
   searchField = By.xpath(`//input[@name='search']`);
 
-
-  isProjectsDisplayed = By.xpath(`//tr[td[.='']]//div[.='Evizi-Hung-Training']`);
-
+  isProjectsDisplayed = By.xpath(
+    `//tr[td[.='']]//div[.='Evizi-Hung-Training']`
+  );
 
   //implement method for SearchProjectsByName
 
-  public async isCurrentProjectPage() {
-    await this.driver.findElement(this.isProjectsDisplayed).isDisplayed();
+  public async clickProjectsTopHead() {
+    await this.driver.findElement(this.projectsTopHeadBtn).click();
+  }
+
+  public async clickViewAllProjectBtn() {
+    await this.driver.findElement(this.viewAllProjectsPageBtn).click();
   }
 
   public async enterNameProject(name: string) {
     await this.driver.findElement(this.searchField).sendKeys(name);
   }
 
-  public async isProjectNameDisplayed(nameProjectInSearchField: string) {
-    await this.driver
-      .findElement(this.isProjectsDisplayed)
-      .sendKeys(nameProjectInSearchField);
+  public async isProjectNameDisplayed() {
+    await this.driver.findElement(this.isProjectsDisplayed).isDisplayed()
   }
 }
--- src/step-definitions/search_project_name.step.ts
+++ src/step-definitions/search_project_name.step.ts
@@ -1,36 +1,38 @@
-// import { Given, When, Then } from "cucumber";
-// import { SearchProjectsByNamePage } from "../page/search_project_name.page";
-// import assert = require("assert");
-// require("chromedriver");
+import { Given, When, Then, World } from "cucumber";
+import { SearchProjectsByNamePage } from "../page/search_project_name.page";
+import assert from "assert";
+require("chromedriver");
 
-// let searchProjectsByName: SearchProjectsByNamePage;
+let searchProjectsByName: SearchProjectsByNamePage;
 
-// Given(/^User click Projects button in top header/, async function () {
-//   searchProjectsByName = new SearchProjectsByNamePage(this.driver);
-//   await searchProjectsByName.isCurrentProjectPage();
-// });
+Given(
+  /^User click Projects button in top header/,
+  async function (this: World) {
+    searchProjectsByName = new SearchProjectsByNamePage(this.driver);
+    await searchProjectsByName.clickProjectsTopHead();
+    await this.driver.sleep(3000);
+  }
+);
 
-// When(/^User click View all projects item/, async function () {
-//   searchProjectsByName = new SearchProjectsByNamePage(this.driver);
-//   await searchProjectsByName.isCurrentProjectPage();
-// });
+When(/^User click View all projects item/, async function (this: World) {
+  searchProjectsByName = new SearchProjectsByNamePage(this.driver);
+  await searchProjectsByName.clickViewAllProjectBtn();
+  await this.driver.sleep(3000);
+});
 
-// When(
-//   /^User enter "([^"]*)" name project in search field/,
-//   async function (nameOfProject: string) {
-//     searchProjectsByName = new SearchProjectsByNamePage(this.driver);
-//     await searchProjectsByName.enterNameProject(nameOfProject);
-//     await this.driver.sleep(4000);
-//   }
-// );
+When(
+  /^User enter "([^"]*)" name project in search field/,
+  async function (nameOfProject: string) {
+    searchProjectsByName = new SearchProjectsByNamePage(this.driver);
+    await searchProjectsByName.enterNameProject(nameOfProject);
+    await this.driver.sleep(3000);
+  }
+);
 
-// Then(
-//   /^Projects with name in search field displays/,
-//   async function (nameProjectInSearchField: string) {
-//     let projectTemplate = new SearchProjectsByNamePage(this.driver);
-//     let isOnProjectPageCreate = await projectTemplate.isProjectNameDisplayed(
-//       nameProjectInSearchField
-//     );
-//     assert.equal(isOnProjectPageCreate, true);
-//   }
-// );
+Then(
+  /^Projects with name in search field displays/,
+  async function (this: World) {
+    searchProjectsByName = new SearchProjectsByNamePage(this.driver);
+    await searchProjectsByName.isProjectNameDisplayed();
+  }
+);
